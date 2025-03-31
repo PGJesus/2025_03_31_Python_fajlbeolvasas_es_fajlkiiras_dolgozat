92a2e25a26b9e84a4e7117208531d8266811dd52
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2025-03-31 09:08:54 +0200

    half done

--- forma1.py
+++ forma1.py
@@ -8,9 +8,26 @@ Olvasd be az f1.txt adatait, majd oldd meg az alábbi feladatokat!
 
 A megoldott feladatokat a kiirt_adatok nevű mappába hozd létre statisztika.txt néven!
 """
+adatok = []
+szam = 0
 
+with open('beolvasando_adatok/f1.txt', 'r', encoding='utf-8') as forrasfajl:
+    for sor in forrasfajl:
+        nev, csapat, wins, done_matches = sor.strip().split(';')
+        # print(nev, csapat, wins, done_matches)
+        adatok.append([nev, csapat, int(wins), int(done_matches)])
 
-print("A beolvasott fájlban összesen ____ versenyző szerepel.")
-print("A legtöbb futamot nyert versenyző: ____")
-print("A legtöbb futamot teljesített versenyző: ____")
+    for sor in adatok:
+        szam += 1
+
+legtobbet_nyert = max(adatok, key=lambda x: x[2])
+
+legtobbet_versenyzet = max(adatok, key=lambda x: x[3])
+
+osszes_futamszam = sum(adatok, key=lambda x: x[3])
+print(osszes_futamszam)
+
+print(f"A beolvasott fájlban összesen {szam} versenyző szerepel.")
+print(f"A legtöbb futamot nyert versenyző: {legtobbet_nyert[0]}")
+print(f"A legtöbb futamot teljesített versenyző: {legtobbet_versenyzet[0]}")
 print("Az átlagos futamszám: ____")
